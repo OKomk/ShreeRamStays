618200f1f86ef0c18d5e2a978d2107fac66cad2d
Om Kshatriya <om@Oms-MacBook-Pro.local>
2025-11-09 11:42:41 +0530

    changed base to ShreeRamStays

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
+  base: "/ShreeRamStays/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
